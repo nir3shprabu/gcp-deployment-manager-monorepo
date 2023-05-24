79c03ac4220386379d73affab4c1f6cddf96002f
nir3shprabu <115976526+nir3shprabu@users.noreply.github.com>
2023-05-24 20:59:26 +0530

    feat: Add dependencies and semantic-release workflow

--- sympl/install-sympl.sh
+++ sympl/install-sympl.sh
@@ -2,5 +2,7 @@
 sudo apt install wget
 sudo wget -P /tmp https://gitlab.com/sympl.io/install/-/raw/master/install.sh
 sudo chmod +x /tmp/install.sh
+# Setting this terminal to gcp debian instance's terminal(xterm-256color)
+# https://stackoverflow.com/questions/24676687/top-xterm-unknown-terminal-type
 export TERM=xterm-256color
 sudo bash /tmp/install.sh --noninteractive 
